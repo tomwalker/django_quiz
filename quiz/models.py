1f5bf973c809279fb1aa3717569728844c49105e
Cody Heimberger <rcodyheimberger@gmail.com>
2014-07-30 15:44:21 -0600

    added support for figures in true_false and multiple choice questions

--- quiz/models.py
+++ quiz/models.py
@@ -463,7 +463,13 @@ class Question(models.Model):
                                  blank=True,
                                  null=True)
 
-    content = models.CharField(max_length=1000,
+    figure = models.ImageField(upload_to='uploads/%Y/%m/%d', 
+                                 blank=True, 
+                                 null=True)
+
+    figure = models.ImageField(upload_to='uploads/%Y/%m/%d',
+                                 blank=True, 
+                                 null=True)
                                blank=False,
                                help_text="Enter the question text that "
                                          "you want displayed",
