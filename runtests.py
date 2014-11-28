67eaccdc73ff1a5114c58f3aa480e094b81cb393
Tom Walker <tomwalker0472@gmail.com>
2014-11-28 14:12:11 +0000

    updated tox to run with python 2.7, 3.3 and 3.4. Tests done on Django 1.5, 1.6 and 1.7

--- runtests.py
+++ runtests.py
@@ -4,11 +4,14 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'test-settings'
 test_dir = os.path.dirname(__file__)
 sys.path.insert(0, test_dir)
 
+import django
 from django.test.utils import get_runner
 from django.conf import settings
 
 
 def runtests():
+    if django.VERSION >= (1, 7):
+        django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner(verbosity=1, interactive=True)
     failures = test_runner.run_tests(
