927e9bb3fe42f10e6cce7693c421045fa3317648
Pete Graham <git@petegraham.co.uk>
2017-06-22 11:30:12 +0100

    Bump requirements

--- setup.py
+++ setup.py
@@ -28,9 +28,9 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     install_requires=[
-        'django-model-utils == 2.0.3',
-        'Django >= 1.5.1',
-        'Pillow == 2.5.0'
+        'django-model-utils == 3.0.0',
+        'Django >= 1.11',
+        'Pillow==4.1.1'
     ],
     test_suite='runtests.runtests'
 )
